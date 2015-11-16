1717fe46e747f4db99b2b5b7b682f0ac1d4ed8af
Crane Bro's <skylerc15@SkylerCranesMBP.ad.uvu.edu>
2015-11-16 10:27:37 -0700

    Update 11/16/15

---
+++ Assets/Scripts/Instantiate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Instantiate : MonoBehaviour {
+
+	public Transform duck;
+	
+	public void SpawnDuck()
+	{
+		Instantiate(duck, new Vector3(0,-3, 0), Quaternion.identity); 
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
