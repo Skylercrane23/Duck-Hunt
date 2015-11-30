7925a69ef302b54f0bc762adf44550821ae6c38c
Crane Bro's <skylerc15@SkylerCranesMBP.ad.uvu.edu>
2015-11-30 09:49:04 -0700

    Update Duck Hunt Movement

---
+++ Assets/Scripts/DirectionChanger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectionChanger : MonoBehaviour {
+
+	public enum Changer {Horizontal, Vertical};
+	public Changer direction; 
+
+	// Use this for initialization
+	//void Start () {
+	
+	//}
+	
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+	
+	public void OnCollisionEnter(Collision hit)
+	{
+		if(hit.transform.tag == "Duck")
+		{
+			DuckMovement movement = hit.transform.GetComponent<DuckMovement>();
+			
+			if(direction == Changer.Horizontal)
+			{
+				movement.DirectionChanger(new Vector3(-1f, 1, 0));
+			}
+			else if(direction == Changer.Vertical)
+			{
+				movement.DirectionChanger(new Vector3(1f, -1, 0));
+			}
+		}
+	}	
+}
--- Assets/Scripts/DuckMovement.cs
+++ Assets/Scripts/DuckMovement.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DuckMovement : MonoBehaviour {
+public class DuckMovement : MonoBehaviour 
+{
+	public float speed;
+	public Vector3 Direction;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		RandomDirection();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		transform.position = transform.position + (Direction * speed);
+	}
 	
+	public void RandomDirection()
+	{
+		Direction = new Vector3(Random.Range(-1f, 1f), Random.Range(.2f, 1f), 0);
+	}
+	
+	public void DirectionChanger(Vector3 _dir)
+	{
+		Direction = new Vector3(Direction.x * _dir.x, Direction.y * _dir.y, 0);
 	}
 }
